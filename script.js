ecc568c65776281354e3aab4180057924bd4075e
griphax <migueldlr88@gmail.com>
2024-10-30 14:42:31 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function countdownTimer() {
+    const endDate = new Date("October 30, 2024 19:59:59").getTime();
+    const timer = setInterval(function() {
+        const now = new Date().getTime();
+        const distance = endDate - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+
+        if (distance < 0) {
+            clearInterval(timer);
+            document.getElementById("countdown").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
+
+countdownTimer();
